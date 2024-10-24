f940c83b51857825dbed163e77e1f194574eaf7c
shixuewen <friendlysxw@163.com>
2024-10-24 13:34:24 +0800

    fix: walletconnect compilation warning

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, context) => {
+    if (config.plugins) {
+      // see: https://github.com/WalletConnect/walletconnect-monorepo/issues/1908#issuecomment-1712429322
+      config.plugins.push(
+        new context.webpack.IgnorePlugin({
+          resourceRegExp: /^(lokijs|pino-pretty|encoding)$/,
+        })
+      );
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
