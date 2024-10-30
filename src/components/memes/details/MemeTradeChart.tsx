07baf2a0092b18d33427b7c06ba4edde4f3e126e
shixuewen <friendlysxw@163.com>
2024-10-30 13:52:40 +0800

    feat: meme graduation

--- src/components/memes/details/MemeTradeChart.tsx
+++ src/components/memes/details/MemeTradeChart.tsx
@@ -4,15 +4,40 @@ import { MemeData } from "@/services/meme/types";
 import MemeChart, { MemeChartData, mockMemeChartdata } from "./MemeChart";
 import useLoadMemeOhlct from "@/hooks/trade/useLoadMemeOhlct";
 import { useEffect } from "react";
+import { PGF_CONTRACT_CHAIN_ID } from "@/constants/pgf";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs-underline";
+
+const getDextoolsChainId = (chainId: number) => {
+  // ether, base,
+  switch (chainId) {
+    case 8453:
+      return "base";
+    case 1:
+      return "ether";
+    default:
+      return "";
+  }
+};
+const getDexWidgetChartUrl = ({
+  chainId,
+  poolAddress,
+}: {
+  chainId: number;
+  poolAddress: string;
+}) => {
+  const chainID = getDextoolsChainId(chainId);
+  if (!chainID || !poolAddress) {
+    return "";
+  }
+  return `https://www.dextools.io/widget-chart/en/${chainID}/pe-light/${poolAddress}?theme=dark&chartType=1&chartResolution=30&drawingToolbars=false&chartInUsd=true`;
+};
 
 export default function MemeTradeChart({ meme }: { meme: MemeData }) {
-  const address = meme.address;
-  const { ohlct, pending, loadMemeOhlct } = useLoadMemeOhlct({
-    address: address,
-  });
-  useEffect(() => {
-    loadMemeOhlct();
-  }, []);
   return (
     <div className="w-full flex flex-col justify-start items-start gap-6">
       <div className="justify-start items-center gap-6 inline-flex">
@@ -36,15 +61,79 @@ export default function MemeTradeChart({ meme }: { meme: MemeData }) {
           </div>
         </div>
       </div>
-      <div className="w-full h-[642px]">
-        {pending ? (
-          <div className="flex justify-center items-start gap-6">
-            Loading...
-          </div>
-        ) : (
-          <MemeChart data={ohlct as MemeChartData} />
-        )}
-      </div>
+      {meme?.graduation?.poolAddress ? (
+        <GraduationAfterChart meme={meme} />
+      ) : (
+        <GraduationBeforeChart meme={meme} />
+      )}
     </div>
+  );
+}
+
+function GraduationBeforeChart({ meme }: { meme: MemeData }) {
+  const { address } = meme;
+  const { ohlct, pending, loadMemeOhlct } = useLoadMemeOhlct({
+    address: address,
+  });
+  useEffect(() => {
+    loadMemeOhlct();
+  }, []);
+  if (pending) {
+    return (
+      <div className="flex justify-center items-start gap-6">Loading...</div>
+    );
+  }
+  return (
+    <div className="w-full h-[642px]">
+      <MemeChart data={ohlct as MemeChartData} />
+    </div>
+  );
+}
+
+function GraduationAfterChart({ meme }: { meme: MemeData }) {
+  const { address } = meme;
+  const { ohlct, pending, loadMemeOhlct } = useLoadMemeOhlct({
+    address: address,
+  });
+  const dexUrl = getDexWidgetChartUrl({
+    chainId: PGF_CONTRACT_CHAIN_ID,
+    poolAddress: meme.graduation?.poolAddress || "",
+  });
+  useEffect(() => {
+    loadMemeOhlct();
+  }, []);
+  if (pending) {
+    return (
+      <div className="flex justify-center items-start gap-6">Loading...</div>
+    );
+  }
+  return (
+    <Tabs defaultValue="after" className="w-full h-full">
+      <TabsList className="w-full mb-6 flex flex-row">
+        <TabsTrigger value={"after"} className="text-[24px] h-[38px] flex-1">
+          Uniswap
+        </TabsTrigger>
+        <TabsTrigger value={"before"} className="text-[24px] h-[38px] flex-1">
+          Seed Round
+        </TabsTrigger>
+      </TabsList>
+
+      <TabsContent value={"after"}>
+        <div className="w-full h-[642px]">
+          <iframe className="w-full h-full" src={dexUrl} />
+        </div>
+      </TabsContent>
+      <TabsContent value={"before"}>
+        <div className="w-full h-[642px]">
+          {pending ? (
+            <div className="flex justify-center items-start gap-6">
+              Loading...
+            </div>
+          ) : (
+            <MemeChart data={ohlct as MemeChartData} />
+          )}
+        </div>
+      </TabsContent>
+    </Tabs>
   );
 }
