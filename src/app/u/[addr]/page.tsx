51c889184964bb077adadaa4252d74741f68ad9a
bufan <bufan@hotmail.com>
2024-11-04 15:56:53 +0800

    mobile view for user detail page

--- src/app/u/[addr]/page.tsx
+++ src/app/u/[addr]/page.tsx
@@ -3,16 +3,27 @@
 import UserBaseInfo from "@/components/user/details/UserBaseInfo";
 import UserTabs from "@/components/user/details/UserTabs";
 import { useParams } from "next/navigation";
+import { isMobile } from "react-device-detect";
 
 export default function UserDetails() {
   const params = useParams<{ addr: string }>();
   const addr = params.addr;
-  return (
-    <div className="flex justify-center items-start gap-6">
-      <UserBaseInfo address={addr} />
-      <div className="flex-1 grow shrink basis-0 self-stretch flex-col justify-start items-start gap-6 flex">
+  if (isMobile)
+    return (
+      <div className="flex-col justify-start items-start gap-6 flex">
+        <UserBaseInfo address={addr} />
         <UserTabs address={addr} />
       </div>
-    </div>
-  );
+    );
+  else
+    return (
+      <div className="flex flex-row justify-center items-start gap-6">
+        <div className="w-[400px]">
+          <UserBaseInfo address={addr} />
+        </div>
+        <div className="flex-1">
+          <UserTabs address={addr} />
+        </div>
+      </div>
+    );
 }
