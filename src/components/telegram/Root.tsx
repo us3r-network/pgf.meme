5b5e7b8cc9ac87f17c55df594470f67091efe044
bufan <bufan@hotmail.com>
2024-11-04 11:06:59 +0800

    warp telegram

---
+++ src/components/telegram/Root.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { type PropsWithChildren, useEffect } from 'react';
+import {
+  initData,
+  miniApp,
+  useLaunchParams,
+  useSignal,
+} from '@telegram-apps/sdk-react';
+import { AppRoot } from '@telegram-apps/telegram-ui';
+
+import { ErrorBoundary } from './ErrorBoundary';
+import { ErrorPage } from './ErrorPage';
+import { useTelegramMock } from '@/hooks/telegram/useTelegramMock';
+import { useDidMount } from '@/hooks/telegram/useDidMount';
+import { useClientOnce } from '@/hooks/telegram/useClientOnce';
+import { init } from './init';
+
+import './styles.css';
+
+function RootInner({ children }: PropsWithChildren) {
+  const isDev = process.env.NODE_ENV === 'development';
+
+  // Mock Telegram environment in development mode if needed.
+  if (isDev) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useTelegramMock();
+  }
+
+  const lp = useLaunchParams();
+  const debug = isDev || lp.startParam === 'debug';
+
+  // Initialize the library.
+  useClientOnce(() => {
+    init(debug);
+  });
+
+  const isDark = useSignal(miniApp.isDark);
+  const initDataUser = useSignal(initData.user);
+
+  // Enable debug mode to see all the methods sent and events received.
+  useEffect(() => {
+    debug && import('eruda').then((lib) => lib.default.init());
+  }, [debug]);
+
+  return (
+      <AppRoot
+        appearance={isDark ? 'dark' : 'light'}
+        platform={['macos', 'ios'].includes(lp.platform) ? 'ios' : 'base'}
+      >
+        {children}
+      </AppRoot>
+  );
+}
+
+export function Root(props: PropsWithChildren) {
+  // Unfortunately, Telegram Mini Apps does not allow us to use all features of
+  // the Server Side Rendering. That's why we are showing loader on the server
+  // side.
+  const didMount = useDidMount();
+
+  return didMount ? (
+    <ErrorBoundary fallback={ErrorPage}>
+      <RootInner {...props}/>
+    </ErrorBoundary>
+  ) : <div className="root__loading">Loading</div>;
+}
