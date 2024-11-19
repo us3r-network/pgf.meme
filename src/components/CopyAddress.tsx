2dcbeb26017d71b5a325cfc435fa02250d7b15a2
shixuewen <friendlysxw@163.com>
2024-11-19 17:18:18 +0800

    feat: about

--- src/components/CopyAddress.tsx
+++ src/components/CopyAddress.tsx
@@ -11,18 +11,18 @@ export default function CopyAddress({
 }) {
   const { toast } = useToast();
   const copyEl = (
-    <div className="flex flex-row items-center gap-2 line-clamp-1">
+    <div
+      className="flex flex-row items-center gap-2 line-clamp-1 w-fit cursor-pointer "
+      onClick={() => {
+        navigator.clipboard.writeText(address);
+        toast({
+          title: "Address copied",
+          description: address,
+        });
+      }}
+    >
       <span className="font-normal">{shortPubKey(address)}</span>
-      <Copy
-        className=" cursor-pointer size-6"
-        onClick={() => {
-          navigator.clipboard.writeText(address);
-          toast({
-            title: "Address copied",
-            description: address,
-          });
-        }}
-      />
+      <Copy className=" size-6" />
     </div>
   );
   if (!label) {
