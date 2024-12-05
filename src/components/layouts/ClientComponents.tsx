500715ee8b24577898aa53ef286a083da987c76f
shixuewen <friendlysxw@163.com>
2024-12-05 20:57:48 +0800

    feat: update ui

--- src/components/layouts/ClientComponents.tsx
+++ src/components/layouts/ClientComponents.tsx
@@ -42,7 +42,6 @@ export function DefaultHeader() {
             >
               <img src="/images/logo.png" className="size-12 max-sm:size-10" />
               <span className="text-primary-foreground text-4xl font-bold max-sm:text-2xl">
-                <span className="max-sm:hidden">Welcome to </span>{" "}
                 castcoin.fun✨
               </span>
             </Link>
