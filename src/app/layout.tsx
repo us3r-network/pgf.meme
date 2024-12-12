b0bdee2588f694ca7523ade97571a7189fc9216a
shixuewen <friendlysxw@163.com>
2024-12-12 17:18:02 +0800

    fix: app name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ import { Comic_Neue as ComicNeueFont } from "next/font/google";
 import Script from "next/script";
 
 export const metadata: Metadata = {
-  title: "degencast.wtf",
-  description: "Welcome to degencast.wtf✨",
+  title: "degencast.fun",
+  description: "Welcome to degencast.fun✨",
 };
 
 const font = ComicNeueFont({
