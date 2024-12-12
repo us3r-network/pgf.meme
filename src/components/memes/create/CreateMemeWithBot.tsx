e9ee2bd7cecf43da0238670f9f91f65269a58ec3
shixuewen <friendlysxw@163.com>
2024-12-12 17:28:23 +0800

    feat: bot name

--- src/components/memes/create/CreateMemeWithBot.tsx
+++ src/components/memes/create/CreateMemeWithBot.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 const botText = `
-  @bot Launch a new token!
+  @degencast.eth Launch a new token!
   Name: memename
   Symbol: SYMBOL
 
